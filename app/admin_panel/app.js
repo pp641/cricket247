ef5f90708e6684ff596151e0f62c0572a47a5158
Prajjwal Pandey <43077034+pp641@users.noreply.github.com>
2024-07-13 21:15:34 +0530

    Server optimization using cluster

--- app/admin_panel/app.js
+++ app/admin_panel/app.js
@@ -1,12 +1,30 @@
 const createError = require('http-errors');
+const http = require('http')
+var debug = require('debug')('temp:server');
+
 const express = require('express');
+var app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const dbConfig = require('../db.js');
-var mongoose = require('mongoose');
+const cluster = require('node:cluster');
+const dbConfig = require('../db');
 const cookieParser = require('cookie-parser');
-var app = express();
+const numCPUs = require('node:os').availableParallelism();
+const process = require('node:process');
+const mongoose = require('mongoose')
+
+async function mongooseConnection(){
+  await mongoose.set('useNewUrlParser', true);
+  await mongoose.set('useFindAndModify', false);
+  await mongoose.set('useCreateIndex', true);
+  await mongoose.set('useUnifiedTopology', true);
+  await mongoose.connect(dbConfig.url,dbConfig.options)
+    .then(() =>  console.log('connection succesful'))
+    .catch((err) => console.error(err));
+  }
+
+  
 
 const corsOptions = {
   origin: '*', // Allow all origins
@@ -23,18 +41,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json());
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect(dbConfig.url,dbConfig.options)
-  .then(() =>  console.log('connection succesful'))
-  .catch((err) => console.error(err));
-
-
-
-
-
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 var authRouter = require('./routes/auth');
@@ -44,9 +50,6 @@ var compression = require('compression')
 
 
 app.use(compression())
-
-
-
 var socket_io    = require( "socket.io" );
 var io           = socket_io();
 app.io = io;
@@ -66,15 +69,8 @@ app.use(function(req, res, next){
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.disable('x-powered-by')
-
-
-
-
-
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/admin_panel', indexRouter);
 app.use('/admin_panel/api', apiRouter);
 app.use('/admin_panel/auth', authRouter);
@@ -128,11 +124,89 @@ app.get('*',function (req,res) {
   res.status(404).send(html);
 });
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
    next(createError(404));
 
 });
 
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
 
-module.exports = app;
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+}else{
+
+  mongooseConnection();
+  var port = normalizePort('3025');
+  app.set('port', port);
+  var server = http.createServer(app);
+  server.listen(port);
+  app.io.attach(server);
+  server.on('error', onError);
+  server.on('listening', onListening);
+  console.log(`Worker ${process.pid} started`);
+}
+
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+
+
+
+
+
+
+
+
+
